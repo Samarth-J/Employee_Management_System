04d297424fd25e2bc417602e77f54cad801594b5
Samarth-J <samarthmj123@gmail.com>
2026-08-06 10:10:40 +0530

    User Registration2

--- src/main/java/com/SpringBootProject/Employee_Management_System/controller/UserController.java
+++ src/main/java/com/SpringBootProject/Employee_Management_System/controller/UserController.java
@@ -1,4 +1,25 @@
 package com.SpringBootProject.Employee_Management_System.controller;
 
+import com.SpringBootProject.Employee_Management_System.dto.RegisterRequest;
+import com.SpringBootProject.Employee_Management_System.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    private String register(@RequestBody RegisterRequest registerRequest){
+        return userService.register(registerRequest);
+    }
+
 }
+
