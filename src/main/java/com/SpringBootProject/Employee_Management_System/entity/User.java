eefa1cbca1df82a99d4fa1fe06c3f4a61c0e5acc
Samarth-J <samarthmj123@gmail.com>
2026-08-06 09:15:37 +0530

    User Registration2

--- src/main/java/com/SpringBootProject/Employee_Management_System/entity/User.java
+++ src/main/java/com/SpringBootProject/Employee_Management_System/entity/User.java
@@ -1,4 +1,24 @@
 package com.SpringBootProject.Employee_Management_System.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @NotNull
+    private String name;
+    @Email
+    private String email;
+    @Size(min=6)
+    private String password;
+    private String role;
+    private boolean verified;
 }
--- src/main/java/com/SpringBootProject/Employee_Management_System/service/UserService.java
+++ src/main/java/com/SpringBootProject/Employee_Management_System/service/UserService.java
@@ -1,4 +1,34 @@
 package com.SpringBootProject.Employee_Management_System.service;
 
+import com.SpringBootProject.Employee_Management_System.dto.RegisterRequest;
+import com.SpringBootProject.Employee_Management_System.entity.User;
+import com.SpringBootProject.Employee_Management_System.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String register(RegisterRequest registerRequest){
+       Optional<User> ou = userRepository.findByEmail(registerRequest.getEmail());
+       if(ou.isPresent()){
+           return "email_id already exists";
+       }
+       else {
+           User user =new User();
+           user.setName(registerRequest.getName());
+           user.setEmail(registerRequest.getEmail());
+           user.setPassword(registerRequest.getPassword());
+           user.setRole("USER_ROLE");
+           user.setVerified(false);
+           userRepository.save(user);
+           return "please enter the otp";
+       }
+    }
 }
